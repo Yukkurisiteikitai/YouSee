01e1360e4cfdfbb8a50a3da061ee3f980dfe9382
Yukkurisiteikitai <yukkuriorsince@gmail.com>
2025-02-19 13:55:53 +0900

    set/QuestionDB

---
+++ src/Question/app.py
@@ -0,0 +1,8 @@
+UserName = "sakura"
+DB_path = "../DataSave"
+template_path = f"{DB_path}/DB-Base-template.json"
+humanity_save_path = f"{DB_path}/User/{UserName}/humanity.json"
+
+# print(humanity_save_path)
+
+# src/DataSave/User/sakura/humanity.json
